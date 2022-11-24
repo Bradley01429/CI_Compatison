1f36f4874a6e6ee58015e4cb7a028feec2d8f8b1
Bradley Reece <b.reece@ibexinnovations.co.uk>
2022-11-24 13:08:20 +0000

    Add failing test

--- main.cpp
+++ main.cpp
@@ -2,10 +2,10 @@
 
 int main(int argc, char* argv[])
 {
-    if (argc < 1)
+    if (argc != 2)
     {
         std::cout << "Only one arg can be supplied" << std::endl;
-        return 0;
+        return 1;
     }
     std::cout << argv[1] << std::endl;
 }
